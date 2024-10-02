166e3f60509ef2263626669347bfcb13f96e091b
tombo-akizu <akasaka-kotaro@keio.jp>
2024-10-02 02:58:45 -0700

    Applied at assetbundle on webserver

--- Assets/Scripts/ModelBundleLoader.cs
+++ Assets/Scripts/ModelBundleLoader.cs
@@ -1,19 +1,41 @@
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using TMPro;
 
 // Load prefab from AssetBundle and instantiate it.
 public class ModelBundleLoader : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI text;
+
     void Start()
     {
         // Load AssetBundle from local files.
-        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "external"));
+        // var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "external"));
+
+        StartCoroutine(Request());        
+    }
+
+    private IEnumerator Request() 
+    {
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle("http://192.168.50.110:8000/external");
+        text.text = "loading";
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success) {
+            Debug.LogError(www.error);
+            text.text = www.error;
+            yield break;
+        }
+        AssetBundle myLoadedAssetBundle = DownloadHandlerAssetBundle.GetContent(www);
 
         if (myLoadedAssetBundle == null)
         {
-            Debug.Log("Failed to load AssetBundle!");
-            return;
+            Debug.LogError("Failed to load AssetBundle!");
+            text.text = "Failed to load AssetBundle!";
+            yield break;
         }
+        text.text = "?";
 
         // Get fbx as GameObject from loaded AssetBundle.
         var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("model.prefab");
